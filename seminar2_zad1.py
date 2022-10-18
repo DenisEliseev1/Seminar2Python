80e291fd0943427b8957bf53ad213850f8e1f9f4
Eliseev Denis <medved-ed@mail.ru>
2022-10-19 00:40:54 +0800

    seminar 2 zad2

--- seminar2_zad1.py
+++ seminar2_zad1.py
@@ -1,13 +1,14 @@
 #Напишите программу, которая принимает на вход вещественное 
 # или целое число и показывает сумму его цифр. 
 # Через строку нельзя решать.
-
-number = float (input('Введите число - '))
-sum = 0
-while number % 1 != 0:
-    number *= 10
-while number > 0:
-    sum += int (number % 10)
-    number = number // 10
-print (f'Сумма цифр числа равна {sum}')
-
+try:
+    number = float (input('Введите число (если дробь - не более 5 знаков после запятой) - '))
+    sum = 0
+    while number % 1 != 0:
+        number = round (number, 5) * 10
+    while number != 0:
+        sum += int (number % 10)
+        number = number // 10
+    print (f'Сумма цифр числа равна {sum}')
+except:
+    print ('Ошибка - необходимо ввести число (если дробь - не более 5 знаков после запятой)')
