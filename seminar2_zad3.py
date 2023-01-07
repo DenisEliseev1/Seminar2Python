fa4c6577557c881cf7de4254ec0a7c9325f1551d
Eliseev Denis <medved-ed@mail.ru>
2023-01-07 22:28:05 +0800

    add new version 20230107 zad3

--- seminar2_zad3.py
+++ seminar2_zad3.py
@@ -1,30 +1,10 @@
-#Напишите программу, в которой пользователь будет задавать две строки, 
-#а программа - определять количество вхождений одной строки в другой. 
-#COUNT или FIND или SPLIT нельзя юзать! говорил же на семинаре.
-
-def convert_string (s):
-    array = []
-    end = 0
-    s = s + (' ')
-    start = 0
-    for i in range (len(s)):
-        if s[i] == (' ') or s[i] == (','):
-            end = i
-            array.append (s[start:end])
-            start = end+1
-    return array
-
-
-
-list_one = convert_string (input ('Введите строку 1'))
-print (list_one)
-list_two = convert_string (input ('Введите строку 2'))
+# Задайте список из n чисел последовательности (1+1/n)**n и выведите на экран их сумму.
+n = int (input ('Введите N - '))
+seq = []
 sum = 0
-for i in range (len(list_one)):
-    for j in range (len(list_two)):
-        if list_one[i] == list_two [j] != (' '):
-            sum+=1
-if sum == 0:
-    print ('Совпадений не найдено')
-else:
-    print (f'Количестов вхождений строки 1 в строку 2 - {sum}')
+for i in range(1,n+1):
+    n1 = round(((1+1/i)**i), 2)
+    seq.append (n1)
+    sum += n1
+print (f'Последовательность - {seq}')
+print (f'Сумма последовательности - {sum}')
